d6c6e305cca740776fd2549e2e164f8020b55481
huathien23102006-del <huathien23102006@gmail.com>
2026-05-03 22:54:32 +0700

    Add files via upload

---
+++ btth1.sql
@@ -0,0 +1,115 @@
+CREATE DATABASE cine_magic;
+USE cine_magic;
+
+CREATE TABLE movies (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    duration_minutes INT NOT NULL CHECK (duration_minutes > 0),
+    age_restriction INT DEFAULT 0 CHECK (age_restriction IN (0,13,16,18))
+);
+
+CREATE TABLE rooms (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    max_seats INT NOT NULL CHECK (max_seats > 0),
+    status ENUM('active', 'maintenance') DEFAULT 'active'
+);
+
+CREATE TABLE showtimes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    movie_id INT NOT NULL,
+    room_id INT NOT NULL,
+    show_time DATETIME NOT NULL,
+    ticket_price DECIMAL(10,2) NOT NULL CHECK (ticket_price >= 0),
+
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+CREATE TABLE bookings (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    showtime_id INT NOT NULL,
+    customer_name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    booking_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (showtime_id) REFERENCES showtimes(id)
+);
+
+INSERT INTO movies (title, duration_minutes, age_restriction)
+VALUES
+('Avengers: Secret Wars', 150, 13),
+('Conan Movie 27', 110, 13),
+('The Nun 3', 120, 18),
+('Doraemon Movie 2025', 100, 0);
+
+INSERT INTO rooms (name, max_seats, status)
+VALUES
+('Phòng 1', 100, 'active'),
+('Phòng 2', 80, 'maintenance'),
+('Phòng 3', 120, 'active');
+
+INSERT INTO showtimes (movie_id, room_id, show_time, ticket_price)
+VALUES
+(1, 1, '2026-05-01 18:00:00', 90000),
+(2, 3, '2026-05-01 20:00:00', 100000),
+(3, 1, '2026-05-02 21:00:00', 110000),
+(4, 3, '2026-05-02 17:00:00', 80000),
+(1, 1, '2026-05-03 19:00:00', 95000);
+
+INSERT INTO bookings (showtime_id, customer_name, phone)
+VALUES
+(1, 'Nguyen Van A', '0900000001'),
+(1, 'Tran Thi B', '0900000002'),
+(2, 'Le Van C', '0900000003'),
+(2, 'Pham Thi D', '0900000004'),
+(3, 'Hoang Van E', '0900000005'),
+(3, 'Nguyen Thi F', '0900000006'),
+(4, 'Tran Van G', '0900000007'),
+(4, 'Le Thi H', '0900000008'),
+(5, 'Pham Van I', '0900000009'),
+(5, 'Hoang Thi K', '0900000010');
+
+UPDATE rooms
+SET status = 'maintenance'
+WHERE id = 1;
+
+UPDATE showtimes
+SET room_id = 2
+WHERE room_id = 1;
+
+DELETE FROM bookings
+WHERE phone = '0987654321';
+
+DELETE FROM bookings
+WHERE showtime_id IN (
+    SELECT id FROM showtimes WHERE movie_id = 3
+);
+
+DELETE FROM showtimes
+WHERE movie_id = 3;
+
+DELETE FROM movies
+WHERE id = 3;
+
+SELECT *
+FROM movies
+WHERE duration_minutes BETWEEN 90 AND 120;
+
+SELECT *
+FROM bookings
+WHERE showtime_id = 2
+ORDER BY booking_date DESC;
+
+SELECT *
+FROM movies
+WHERE age_restriction = 18
+   OR duration_minutes > 150;
+   
+   SELECT *
+FROM showtimes
+WHERE ticket_price > 100000
+  AND MONTH(show_time) = MONTH(CURRENT_DATE())
+  AND YEAR(show_time) = YEAR(CURRENT_DATE());
+
+
